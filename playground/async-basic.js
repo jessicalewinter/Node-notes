2fe91aa1ab513beb37ca36213531efffc7eeaeba
Jessica Lewinter <jessicalewinter@gmail.com>
2017-10-24 19:24:32 -0300

    Adding a callback.js file

--- playground/async-basic.js
+++ playground/async-basic.js
@@ -1,11 +1,15 @@
 console.log('Starting app');
 
 setTimeout(() => {
-  console.log('Inside of callback');
+  console.log('First setTimeout but third to callback');
 },2000);
 
 setTimeout(() => {
-  console.log('Second setTimeout');
+  console.log('Second setTimeout but first to callback');
 },0);
+
+setTimeout(() => {
+  console.log('Third setTimeout but second to callback');
+},1000);
 
 console.log('Finishing up');
